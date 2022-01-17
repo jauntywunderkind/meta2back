ba280a8ebe5660ea862c475a0bd04b8a6c896ea5
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2022-01-16 19:41:42 -0500

    switch to using BTN_SIDE, which seems to be back.

--- meta2back.c
+++ meta2back.c
@@ -6,16 +6,18 @@ int main(int argc, char *argv[]) {
 	setbuf(stdin, NULL);
 	setbuf(stdout, NULL);
 
-	const int find = argc >= 2 ? atoi(argv[1]) : KEY_LEFTMETA;
-	const int replace = argc >= 3 ? atoi(argv[2]) : KEY_BACK;
+	// codes via https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
+	const __u16 find = argc >= 2 ? atoi(argv[1]) : KEY_LEFTMETA;
+	// didn't do anything: BTN_BACK, 0x116
+	// worked as expected: BTN_SIDE, 0x113
+	const __u16 replace = argc >= 3 ? atoi(argv[2]) : BTN_SIDE;
 
 	struct input_event event;
 	while (fread(&event, sizeof(event), 1, stdin) == 1) {
-		// https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
-		if (event.type == EV_KEY && event.code == find)
+		if (event.type == EV_KEY && event.code == find) {
 			event.code = replace;
+		}
 
 		fwrite(&event, sizeof(event), 1, stdout);
 	}
 }
-
